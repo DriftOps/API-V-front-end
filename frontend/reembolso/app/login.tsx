300e777652509fa29484c322939c45da31f64fa2
Kalil Pereira <kalil.alves.2019@gmail.com>
2025-03-11 12:15:55 -0300

    Tela de login

---
+++ frontend/reembolso/app/login.tsx
@@ -0,0 +1,77 @@
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+export default function Login() {
+  return (
+    <View
+      style={{
+        backgroundColor: "#002963",
+        flex: 2,
+        justifyContent: "space-evenly",
+        alignItems: "center"
+      }}
+    >
+      <Image source={require('../assets/images/gswlogo.svg')}></Image>
+
+      <View style={{
+      }}>
+
+      <Text 
+      style={{
+        color: "white",
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 10
+      }}>Usuário:</Text>
+
+      <TextInput    
+      style={{
+        backgroundColor: "white",
+        borderRadius: 30,
+        height: 50,
+        width: 250
+      }}></TextInput>
+      </View>
+
+      <View>
+      <Text 
+      style={{
+        color: "white",
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 10
+      }}>Senha:</Text>
+
+      <TextInput    
+      style={{
+        backgroundColor: "white",
+        borderRadius: 30,
+        height: 50,
+        width: 250
+      }}></TextInput>
+      </View>
+
+      <TouchableOpacity
+          style={{
+            backgroundColor: "white",
+            borderRadius: 30,
+            height: 50,
+            width: 250, 
+            justifyContent: "center",
+            alignItems: "center", 
+          }}
+        >
+          <Text
+            style={{
+              color: "black", 
+              fontSize: 18, 
+              fontWeight: "bold", 
+            }}
+          >
+            Efetuar Login
+          </Text>
+        </TouchableOpacity>
+      
+    </View>
+  );
+}
